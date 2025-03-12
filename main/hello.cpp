6fced73f1655f0ca78d0d49d708d988ad6fde7e5
M SAI NITHIN <99406663+Nithin6524@users.noreply.github.com>
2025-03-12 22:02:39 +0530

    change is made

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "hello,  PES2UG22CS284";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
